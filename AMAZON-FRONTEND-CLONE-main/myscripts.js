2476731990c61ba94d85eca9735945db51763d49
NARENDRUNI SANDEEP <139003520+Narendruni-Sandeep@users.noreply.github.com>
2024-03-28 22:33:02 +0530

    Add files via upload

---
+++ AMAZON-FRONTEND-CLONE-main/myscripts.js
@@ -0,0 +1,193 @@
+function
+addtocart1 ()
+{
+
+
+  const myElement = document.getElementById ("1").value;
+
+  var my = sessionStorage.getItem (myElement);
+  var myElement1=Number(my);
+	console.log(myElement1);
+  if (myElement == null)
+	{
+
+	  myElement1 = 0;
+
+	}
+
+  else
+	{
+
+	  myElement1 += 1;
+
+	}
+
+  alert (myElement + " added successfully\n Quantity : " + myElement1 + " " 
+		 );
+
+  sessionStorage.setItem (myElement, myElement1);
+
+  let data = sessionStorage.getItem (myElement);
+
+}
+
+function
+addtocart2 ()
+{
+
+
+  const myElement = document.getElementById ("2").value;
+
+  var my = sessionStorage.getItem (myElement);
+  var myElement1=Number(my);
+	console.log(myElement1);
+  if (myElement == null)
+	{
+
+	  myElement1 = 0;
+
+	}
+
+  else
+	{
+
+	  myElement1 += 1;
+
+	}
+
+  alert (myElement + " added successfully\n Quantity : " + myElement1 + " " 
+		 );
+
+  sessionStorage.setItem (myElement, myElement1);
+
+  let data = sessionStorage.getItem (myElement);
+
+}
+function
+addtocart3 ()
+{
+
+
+  const myElement = document.getElementById ("3").value;
+
+  var my = sessionStorage.getItem (myElement);
+  var myElement1=Number(my);
+	console.log(myElement1);
+  if (myElement == null)
+	{
+
+	  myElement1 = 0;
+
+	}
+
+  else
+	{
+
+	  myElement1 += 1;
+
+	}
+
+  alert (myElement + " added successfully\n Quantity : " + myElement1 + " " 
+		 );
+
+  sessionStorage.setItem (myElement, myElement1);
+
+  let data = sessionStorage.getItem (myElement);
+
+}
+function
+addtocart4 ()
+{
+
+
+  const myElement = document.getElementById ("4").value;
+
+  var my = sessionStorage.getItem (myElement);
+  var myElement1=Number(my);
+	console.log(myElement1);
+  if (myElement == null)
+	{
+
+	  myElement1 = 0;
+
+	}
+
+  else
+	{
+
+	  myElement1 += 1;
+
+	}
+
+  alert (myElement + " added successfully\n Quantity : " + myElement1 + " " 
+		 );
+
+  sessionStorage.setItem (myElement, myElement1);
+
+  let data = sessionStorage.getItem (myElement);
+
+}
+function
+addtocart5()
+{
+
+
+  const myElement = document.getElementById ("5").value;
+
+  var my = sessionStorage.getItem (myElement);
+  var myElement1=Number(my);
+	console.log(myElement1);
+  if (myElement == null)
+	{
+
+	  myElement1 = 0;
+
+	}
+
+  else
+	{
+
+	  myElement1 += 1;
+
+	}
+
+  alert (myElement + " added successfully\n Quantity : " + myElement1 + " " 
+		 );
+
+  sessionStorage.setItem (myElement, myElement1);
+
+  let data = sessionStorage.getItem (myElement);
+
+}
+function
+addtocart6 ()
+{
+
+
+  const myElement = document.getElementById ("6").value;
+
+  var my = sessionStorage.getItem (myElement);
+  var myElement1=Number(my);
+	console.log(myElement1);
+  if (myElement == null)
+	{
+
+	  myElement1 = 0;
+
+	}
+
+  else
+	{
+
+	  myElement1 += 1;
+
+	}
+
+  alert (myElement + " added successfully\n Quantity : " + myElement1 + " " 
+		 );
+
+  sessionStorage.setItem (myElement, myElement1);
+
+  let data = sessionStorage.getItem (myElement);
+
+}
